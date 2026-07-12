a504bc6e6f3b6f90ef5a36548c70ddb0ac9ed200
Manoj Kumar <manoj.er.kumar@gmail.com>
2026-07-12 12:25:35 +0530

    functions practice

---
+++ fundaments_programming/functions.py
@@ -0,0 +1,38 @@
+def f1(a):
+    """add 1 to input and return"""
+    b = a+1
+    return b
+
+print(f1(100))
+
+def f2(a):
+    """add 2 to input and return"""
+    b = a+2
+    return b
+
+for i in range(5):
+    print(f2(i))
+
+name='manoj'
+print("length:", len(name))
+
+def mul(a,b):
+    c = a*b
+    print("c", c)
+    return c
+
+print("mul:", mul(8,9))
+#print("mul:", mul('seg',"kumar"))
+
+
+def acdc(y):
+    print(rating)
+    return (rating + y)
+
+rating = 9
+
+print(acdc(1))
+
+L = [1,3,2]
+sorted(L)
+print(L)
